3223a15fba4c91f59d8fd3bcf67d2507d0403e46
Alp Kaan Aksu <alpkaksu@gmail.com>
2020-09-09 12:05:07 +0300

    fix for my personal computer

--- src/genanki/create_deck.py
+++ src/genanki/create_deck.py
@@ -185,7 +185,7 @@ color: gray
                 'qfmt': '<span class="front-text-pre">{{AField}}</span>',
                 'afmt': '<span class="front-text-post">{{AField}}</span>'
                 '<hr id="answer">'
-                '<span class="back-text>"{{BField}}</span>',
+                '<span class="back-text">{{BField}}</span>',
             }
         ],
         css=CSS
